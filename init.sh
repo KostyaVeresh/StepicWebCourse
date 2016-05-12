d1e185645e5dd8227076e8ddbcc20d196c16980c
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-12 20:52:27 +0300

    fixed

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ mkdir public uploads etc
 cd public
 mkdir img css js
 cd
+cp /home/box/StepicWebCourse/web/etc/nginx.conf /home/box/web/etc/nginx.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
