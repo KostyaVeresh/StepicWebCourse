0c7ea64d8463e23ef439c4d135d9e4fec9380b68
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-13 17:17:33 +0300

    structure changed

--- init.sh
+++ init.sh
@@ -1,9 +1,2 @@
-mkdir web
-cd web
-mkdir public uploads etc
-cd public
-mkdir img css js
-cd
-cp /home/box/StepicWebCourse/nginx.conf /home/box/web/etc/nginx.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
