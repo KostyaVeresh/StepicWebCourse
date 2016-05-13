0eb85100de8ae1c81abade1e74940eb3a3767854
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-13 20:43:03 +0300

    fixed hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
-from string import maketrans
-
 def wsgiApp(environ, start_response):
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = (environ.get('wsgi.input')).translate(maketrans('&', '\n'))
+	body = environ['wsgi.input'].read()
 	start_response(status, headers)
 	return [body]
