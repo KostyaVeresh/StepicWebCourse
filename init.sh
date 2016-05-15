c96e40f1b391dafcec9f741af28b02030b544522
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-15 17:24:17 +0300

    gunicorn django start added

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 gunicorn -c /etc/gunicorn.d/hello.py hello:wsgiApp
+gunicorn -b 0.0.0.0:8000 ask.ask.wsgi
