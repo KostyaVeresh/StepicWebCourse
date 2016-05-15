2824f2260bfaf18246eee3e37e1837556f08f56b
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-15 21:06:48 +0300

    fixed everything

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -9,11 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^login/$', test),
-	url(r'^signup/$', test),
-	url(r'^question/(?P<123>)/$', test),
-	url(r'^ask/$', test),
-	url(r'^popular/$', test),
-	url(r'^new/$', test)
-    #url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^', include('qa.urls'))
 )
