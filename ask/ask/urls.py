e735a7bdc91075fcb92668eedcfcd91fbe3360c2
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-15 17:19:34 +0300

    added reg expr for URLs

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,10 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from qa.views import test
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^login/$', test),
+	url(r'^signup/$', test),
+	url(r'^question/(?P<123>)/$', test),
+	url(r'^ask/$', test),
+	url(r'^popular/$', test),
+	url(r'^new/$', test)
+    #url(r'^admin/', include(admin.site.urls)),
 )
