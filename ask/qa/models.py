167e6afd857702fb30e696d3f3789c13300b3f55
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-19 20:05:49 +0300

    fixed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateField(auto_now_add=True)
-	rating = models.IntegerField()
+	rating = models.IntegerField(auto_now_add=True)
 	author = models.ForeignKey(User, related_name='question_author')
 	likes = models.ManyToManyField(User, related_name='question_likes')
 	
