9a71c6cfb3c111ad8668b394eb7739581eeaa327
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-19 19:49:54 +0300

    fixed init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,3 +1,2 @@
 sudo /etc/init.d/mysql restart
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS qadb; CREATE USER 'box'@'localhost'; GRANT ALL ON qadb.* TO 'box'@'localhost';"
-python /home/box/web/ask/manage.py syncdb
