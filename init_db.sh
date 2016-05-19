f0da1b1d3ea11e7eb7ea07f41f3538f89ffe20e0
KostyaVeresh <kostya.veresh@mail.ru>
2016-05-19 17:33:27 +0300

    added syncdb to init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,7 @@
 sudo /etc/init.d/mysql restart
+mysql -uroot -e "DROP DATABASE qadb"
 mysql -uroot -e "CREATE DATABASE qadb"
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '12345'"
-mysql -uroot -e "GRANT ALL ON qadb.* TO 'box'@'localhost'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON qadb.* TO 'box'@'localhost'"
+
+python /home/box/web/ask/manage.py syncdb
